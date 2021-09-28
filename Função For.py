7dddc1d83482d0d5eaacb376906e3d7e7373d5ec
Vitor Mendes Gonçalo <vitor.goncalo@hotmail.com>
2021-09-28 20:05:56 -0300

    Create Função For.py

---
+++ Função For.py
@@ -0,0 +1,11 @@
+while True:
+    Primo = int(input('Digite um numero que iremos dizer quantos numeros primos abaixo dele existem: ')) # Variável Primo aquisita um número
+    Number = int(0)
+    Contagem = int(10) # Variável Contagem aquisita o número inteiro 10
+    for Number in range (Primo+1): # Inseri 0 na variável Number e soma 1 fazendo o comando de imprimir.
+        print(Number)
+    else:
+        print('Acabou')
+    
+        
+    
