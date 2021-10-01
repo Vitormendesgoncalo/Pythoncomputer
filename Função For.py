00d0a20c9cb381f56653d0dc2e19cb9c52b4ad24
Vitor Mendes Gonçalo <vitor.goncalo@hotmail.com>
2021-10-01 18:17:43 -0300

    Change
    Wrote the comments

--- Função For.py
+++ Função For.py
@@ -1,10 +1,10 @@
 import time
-number = int(0)
-start = int(input('Write the first number:'))
-end = int(input('Write the last number:'))
-degree = int(input('Write the degree that it will count:'))
-for number in range(start,end,degree):
-    print(number)
-    time.sleep(1)
+number = int(0)#It´s a number that it will sum the degree
+start = int(input('Write the first number:'))#A number that it will be the start
+end = int(input('Write the last number:'))#A number that it will be the end
+degree = int(input('Write the degree that it will count:'))#It will be how much it will jump
+for number in range(start,end,degree): # Comand for
+    print(number) # Print the variable number
+    time.sleep(1) # wait one second
         
     
