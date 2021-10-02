41cb13c2e7172c888a02ac5b7586ea0437975958
Vitor Mendes Gonçalo <vitor.goncalo@hotmail.com>
2021-10-02 19:24:57 -0300

    Create Dois numeros somados.py

---
+++ Dois numeros somados.py
@@ -0,0 +1,5 @@
+Numberone = int(input('Write the first number: ')) 
+Numbertwo = int(input('Write the second number: '))
+Result = Numberone + Numbertwo
+print(' The sum {} + {} is {}!'.format(Numberone, Numbertwo, Result)) # Change {} to Numberone, Numbertwo, Result        
+    
