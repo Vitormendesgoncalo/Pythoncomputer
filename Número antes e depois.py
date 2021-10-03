a39a029bae76813799565ec4bfdeea6985099689
Vitor Mendes Gonçalo <vitor.goncalo@hotmail.com>
2021-10-03 20:19:39 -0300

    Create Número antes e depois.py

---
+++ Número antes e depois.py
@@ -0,0 +1,9 @@
+#This is one type to solve the problem
+#Value = int(input('Enter an integer value: '))
+#Previous = Value - 1
+#Later = Value + 1
+#print('The previous value is {}, the later value is {} and the original value is {}'.format(Previous, Later, Value))
+
+# Second type to solve the problem
+Value = int(input("Enter an integer value: "))
+print ('The previous value is {}, the later value is {} and the original value is {}'.format(Value-1, Value+2, Value))
