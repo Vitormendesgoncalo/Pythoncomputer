5bd480b75ea9536c0b2b1b0380cb073366a4a0f6
Vitor Mendes Gonçalo <vitor.goncalo@hotmail.com>
2021-10-04 18:42:51 -0300

    Create Transformação de medidas.py

---
+++ Transformação de medidas.py
@@ -0,0 +1,3 @@
+print('We need to show to the user all metric measuares!')
+meters = float(input("Enter the value in meters: "))
+print('In mm is {:.0f} and in cm is {:.0f}'.format(meters*1000, meters*100))
