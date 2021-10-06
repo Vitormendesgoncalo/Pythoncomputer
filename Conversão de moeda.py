bd7b83d11b4f8b877a9c9f1b058ae844b87ccbb2
Vitor Mendes Gonçalo <vitor.goncalo@hotmail.com>
2021-10-06 18:09:21 -0300

    Create Conversão de moeda.py

---
+++ Conversão de moeda.py
@@ -0,0 +1,4 @@
+print('We will show now how much you can take in dollar!')
+Amount = float(input("Enter the amount you have: "))
+Price = float(input("Enter the dollar quote: "))
+print('You have in dollar {:.2f}.'.format(Amount/Price))
