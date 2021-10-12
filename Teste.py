5324a5a98b0aaea88609bae3f950bf222e90fc95
Vitor Mendes Gonçalo <vitor.goncalo@hotmail.com>
2021-10-12 11:52:08 -0300

    Create Teste.py

---
+++ Teste.py
@@ -0,0 +1,14 @@
+def Subir(): #Função subir
+    print("case1") #Imprimi palavra
+
+def Descer(): #Função descer
+    print("case2") #Imprimi palavra
+
+
+while True:
+    Comando = { #Chaves abre o dicionário
+        0:Subir, # (Subir) pertence (:) ao numero (0)
+        1:Descer,# (Descer) pertence (:) ao numero (1)
+        }
+    Comando = Comando.get (int(input('Digite o numero 0 para "Subir" ou 1 para "Descer": '))) # (Comando.get) roda o dicionário aquisitando a palavra através do numero(0 ou 1) para a variável Comando.
+    Comando() #Roda a função escrita na variável Comando
