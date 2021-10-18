66236e01f80d3608d916aeba2d5ad8ec6fea0af5
Vitor Mendes Gonçalo <vitor.goncalo@hotmail.com>
2021-10-17 21:16:45 -0300

    Create Verificar nome Silva.py

---
+++ Verificar nome Silva.py
@@ -0,0 +1,12 @@
+while True:
+    Name = str(input("Write your complete name: ")).strip()
+    print("In your name have Silva?\n{}".format('silva' in Name.lower()))
+
+#while True:
+#    print("Now we going to verify if your name have Silva!")
+#    Name = str(input("Write your complete name: ")).strip()
+#    Name = Name.upper()
+#    Name = Name.split()
+#    for time in range (10):
+#        if Name[time] == "SILVA":
+#            print(Name[time] == "SILVA")
